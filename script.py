c3741e28bff6e743b3cf687c28aac54d8116ebe9
kishorv06 <developer.kishor@gmail.com>
2016-07-01 19:18:07 +0530

    Small tweaks and bug fixes

--- script.py
+++ script.py
@@ -3,8 +3,7 @@ import re
 import urllib
 
 
-
-
+#Function definitions
 def find_between( s, first, last ):
     try:
         start = s.index( first ) + len( first )
@@ -28,37 +27,77 @@ print "#########################################################################
 print "#################################### CUSAT RESULT DOWNLOADER ####################################"
 print "####################################       BY KISHOR V       ####################################"
 print "#################################################################################################"
-print "\n\nOpen a result on the webpage and input values as per the website"
+print "\n\nOpen a result on the webpage and input values as per the website\n"
 
 
 
 #Variable initialisation
-sem = raw_input("Semester : ")
-course = "B.Tech" if (int(input("Degree [ 1. B.Tech | 2. M.Tech ] (Integer) : ")) == 1) else "M.Tech" 
-exam = raw_input("Result type [Regular | Revaluation | Supplementary | etc.] : ")
-month = raw_input("Month : ")
-year = raw_input("Year : ")
-i = int(input("Starting Reg. Number : "))
-last = int(input("Last Reg. Number : "))
-
+courses = ["B.Tech","M.Tech"]
+exams = ["Regular","Revaluation","Supplementary","Improvement"]
+months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 result = ""
 branch = ""
+
+#Getting user inputs
+sem = raw_input("Semester : ")
+
+#Loop until user enters correct input
+while(1):
+	course = int(input("\nDegree [ 1. B.Tech | 2. M.Tech ] (Integer) : "))
+	if(course > 0 and course < 3):
+		course = courses[course - 1]
+		break	
+	else:
+		print "Invalid Entry"
+
+while (1) :
+	exam = int(input("\nResult type [1. Regular | 2. Revaluation | 3. Supplementary | 4. Improvement ] (Integer) : "))
+	if(exam > 0 and exam < 5):
+		exam = exams[exam - 1]
+		break	
+	else:
+		print "Invalid Entry"
+
+while (1) :
+	month = int(input("\nMonth (Integer) :"))
+	if(month > 0 and month < 13):
+		month = months[month - 1]
+		break
+	else:
+		print "Invalid Entry"
+
+year = raw_input("\nYear : ")
+i = int(input("\nStarting Reg. Number : "))
+last = int(input("\nLast Reg. Number : "))
+filename = raw_input("\nOutput Excel Sheet Name [Without extension] : ")
+filename = filename+".xls"
 url = "http://exam.cusat.ac.in/erp5/cusat/CUSAT-RESULT/Result_Declaration/display_sup_result?statuscheck=failed&deg_name="+course+"&semester="+urllib.quote(sem)+"&month="+month+"&year="+year+"&result_type="+exam+"&regno="
             
-print "Downloading...\n\n"
+
+
+#Printing headings
+print "\n\n\nReg. No".ljust(11) + "Name".ljust(30) + "Semester".ljust(15) + "Course".ljust(15) + "Classification"
+print "-"*100 + "\n"
+
 while(i<=last):
     try:
         u = urllib2.urlopen(url+str(i))
+
         #Complete HTML
         text = u.read()
+
         #Replaced Line Breaks
         text = text.replace('\n','')
+
         #Searched for first table ( Name and details )
         data = find_between(text,'<table width="100%" class="order-list" border="3">','</table>')
+
         #Removed first table 
         text = text.replace('<table width="100%" class="order-list" border="3">','',1)
+
         #Searched for second table ( Marks )
         data = data + find_between(text,'<table width="100%" class="order-list" border="2">','</table>')
+
         #Removing unnecessary styling and HTML tags
         data = re.sub(r"state\d{2}", "state", data)
         data = re.sub(r"state\d", "state", data)
@@ -69,17 +108,20 @@ while(i<=last):
         data = replace_all_2(data,replace2)
         data = data.replace("\tPASSED",'')
         data = data.replace("\tFAILED",'')
-        #Getting total and GPA
-        data = data.replace("<b>Subject Code</b>    <b>Subject Name</b> <th>Marks (Grade)</th><th>Result</th>",'')
+
+        #Getting Total and GPA
+        data = data.replace("<b>Subject Code</b>	<b>Subject Name</b>	<th>Marks (Grade)</th><th>Result</th>",'')
         total = find_between(text,'Total :','<br>')
-        gpa = find_between(text,'GPA   :  ','<br>')
-        tgpa = find_between(text,'<b>CGPA&nbsp;:&nbsp;','</b><br>')
-        cls = find_between(text,"<b>Classification&nbsp;:&nbsp;","</b><br>")
+        gpa = find_between(text,'GPA   :','<br>')
+        gpa = gpa + "0"
+        tgpa = find_between(text,'CGPA&nbsp;:&nbsp;','</b><br>')
+        tgpa = tgpa + "0"
+        ennelum= find_between(text,'Classification&nbsp;:&nbsp;','</b><br>')
         clmn = data.split('\t')
         name = ""
         j = 0
         data = ""
-        status = ""
+
         #Removing subject name
         while(j < len(clmn)):
             if ((j == 0) or (j%3 != 0)):
@@ -87,30 +129,40 @@ while(i<=last):
             if (j == 1):
                 name = clmn[1]
             j += 1
-        if(name == "<b>Subject Name</b>"):
-            print "Fetched : " + str(i) + " - Result unavailable"
+	
+	#We don't have a result, check next
+	if(name == "<b>Subject Name</b>" or name == ""):
+            print str(i) + "   Result unavailable"
             i += 1
             continue
-        if ((gpa != "") and (float(gpa) > 0)):
-            status += "[Sem : Passed"
+
+	#Justifying text for printing
+        name = name.ljust(30)
+        toprint = str(i) + "   " + name
+
+	#Checking status
+        if(float(gpa)>0):
+            toprint = toprint + "PASSED".ljust(15)
         else:
-            status += "[Sem : Failed"
-        if ((tgpa != "") and (float(tgpa) > 0)):
-            status += " | Course : Passed]"
-        else:
-            status += " | Course : Failed]"
-        print "Fetched : " + str(i) + " - " + status +" - "+ name
-        #Completely fetched data
-        result  += data+total+'\t'+gpa+"\t"+tgpa+"\t"+cls+"\n"
+            toprint = toprint + "FAILED".ljust(15)
+        if (sem == '8'):
+            if(float(tgpa)>0):
+                toprint = toprint + "PASSED".ljust(15) +ennelum
+            else:
+                toprint = toprint + "FAILED".ljust(15)
+	#Printing to screen
+        print toprint
+
+        result  += data+total+'\t'+gpa+ '\t' +tgpa + '\t' + ennelum +"\n"
     except Exception,e:
         print e
+
     i += 1
 
 #Writing result to file
-with open("Result.txt", "w") as myfile:
-            myfile.write(result)
+with open(filename, "w") as myfile:
+	myfile.write(result)
             
-print "\n\n\nFile exported as text (Result.txt) , Copy-paste it to Excel"
-print "Script by Kishor V"
+print "\n\n\nFile exported as Excel Sheet : " + filename
+print "\n\n\nScript by Kishor V"
 raw_input("Press any key to exit...")
-
